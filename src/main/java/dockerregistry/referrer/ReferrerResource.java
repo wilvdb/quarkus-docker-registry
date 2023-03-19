40c11e8f3a56da9f32f507b126b8a23eb4c0df6d
wil <wilfried.vandenberghe@gmail.com>
2023-03-19 21:38:18 +0100

    add referrers api

---
+++ src/main/java/dockerregistry/referrer/ReferrerResource.java
@@ -0,0 +1,18 @@
+package dockerregistry.referrer;
+
+import dockerregistry.internal.error.exception.UnsupportedException;
+import dockerregistry.internal.validation.Namespace;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/v2/{name}/referrers/{digest}")
+public class ReferrerResource {
+
+    @GET
+    public Response getReferrers(@Namespace @PathParam("name") String name, @PathParam("digest") String digest) {
+        throw new UnsupportedException();
+    }
+}
