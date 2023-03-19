955203f8878fba3e1c212d4479ce5d2dc2fe7118
wil <wilfried.vandenberghe@gmail.com>
2023-03-19 21:44:12 +0100

    add filtering to referrers api

--- src/main/java/dockerregistry/referrer/ReferrerResource.java
+++ src/main/java/dockerregistry/referrer/ReferrerResource.java
@@ -6,13 +6,14 @@ import dockerregistry.internal.validation.Namespace;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 @Path("/v2/{name}/referrers/{digest}")
 public class ReferrerResource {
 
     @GET
-    public Response getReferrers(@Namespace @PathParam("name") String name, @PathParam("digest") String digest) {
+    public Response getReferrers(@Namespace @PathParam("name") String name, @PathParam("digest") String digest, @QueryParam("artifactType") String artifactType) {
         throw new UnsupportedException();
     }
 }
