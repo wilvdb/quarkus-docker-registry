becb2eb163381d3253365c356ebdf2e738f081d8
wil <wilfried.vandenberghe@gmail.com>
2023-04-22 19:20:49 +0200

    remove file storage from ManifestService

--- src/main/java/dockerregistry/manifest/ManifestResource.java
+++ src/main/java/dockerregistry/manifest/ManifestResource.java
@@ -1,14 +1,15 @@
 package dockerregistry.manifest;
 
+import dockerregistry.internal.error.exception.NameUnknownException;
 import dockerregistry.internal.error.exception.UnsupportedException;
 import dockerregistry.internal.rest.ResponseBuilder;
 import dockerregistry.internal.validation.Namespace;
 import dockerregistry.internal.validation.Reference;
-
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
+
 import java.io.InputStream;
 import java.util.Optional;
 
@@ -21,13 +22,13 @@ public class ManifestResource {
     @RolesAllowed({"read"})
     @GET
     public Response download(@Namespace @PathParam("name") String name, @Reference @PathParam("reference") String reference) {
-        manifestService.checkExistence(name, reference);
-
-        return ResponseBuilder.ok(manifestService.getContent(name, reference))
-                    .dockerContentDigest("sha256:" + manifestService.getSha256(name, reference))
-                    .contentType(manifestService.getMediaType(name, reference))
-                    .contentLength(manifestService.getContentLength(name, reference))
-                    .build();
+        return manifestService.getManifest(name, reference).
+                map(manifest -> ResponseBuilder.ok()
+                        .dockerContentDigest(manifest.digest())
+                        .contentLength(manifest.length())
+                        .contentType(manifest.mediaType())
+                        .build())
+                .orElseThrow(NameUnknownException::new);
     }
 
     @RolesAllowed({"write"})
--- src/main/java/dockerregistry/manifest/ManifestService.java
+++ src/main/java/dockerregistry/manifest/ManifestService.java
@@ -1,23 +1,18 @@
 package dockerregistry.manifest;
 
 import dockerregistry.internal.digest.DigestService;
-import dockerregistry.internal.error.exception.ManifestInvalidException;
-import dockerregistry.internal.error.exception.NameUnknownException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import dockerregistry.internal.error.exception.ManifestBlobUnknownException;
-
-import jakarta.annotation.PostConstruct;
+import dockerregistry.internal.error.exception.ManifestInvalidException;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.json.Json;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -26,18 +21,11 @@ public class ManifestService {
 
     private static final Logger logger = LoggerFactory.getLogger(ManifestService.class);
 
-    private Path path;
-
     @Inject
     DigestService digestService;
 
     @Inject
     ManifestRepository repository;
-
-    @PostConstruct
-    protected void createRegistryDirectory() throws IOException {
-        path = Files.createTempDirectory("tmpRegistry");
-    }
 
     @Transactional
     public Optional<Manifest> getManifest(String name, String reference) {
@@ -55,21 +43,16 @@ public class ManifestService {
     public Manifest saveManifest(String name, String reference, InputStream input) {
         logger.debug("Save manifest for image {} and tag {}", name, reference);
 
-        var tagPath = path.resolve(name + "." + reference + ".json");
-
-
-        try (var outputChannel = Files.newByteChannel(tagPath, StandardOpenOption.APPEND, StandardOpenOption.CREATE)) {
-            logger.debug("Write manifest to file {}", tagPath);
+        try {
             byte[] content = input.readAllBytes();
-            outputChannel.write(ByteBuffer.wrap(content));
 
             var manifest = new ManifestEntity();
             manifest.setCreatedAt(LocalDateTime.now());
             manifest.setName(name);
             manifest.setTag(reference);
             manifest.setDigest(digestService.getDigest(content));
-            manifest.setLength(Files.size(tagPath));
-            manifest.setMediaType(getMediaType(name, reference));
+            manifest.setLength(content.length);
+            manifest.setMediaType(getMediaType(content));
             manifest.setContent(content);
             repository.persist(manifest);
 
@@ -79,36 +62,8 @@ public class ManifestService {
         }
     }
 
-    public String getSha256(String name, String reference) {
-        logger.debug("Get manifest digest for image {} with reference {}", name, reference);
-
-        try {
-            var tagPath = path.resolve(name + "." + reference + ".json");
-
-            return digestService.getDigest(Files.readAllBytes(tagPath));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-
-    public long getContentLength(String name, String reference) {
-        logger.debug("Get manifest content size for image {} with reference {}", name, reference);
-
-        try {
-            return Files.size(path.resolve(name + "." + reference + ".json"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public String getMediaType(String name, String reference) {
-        logger.debug("Get manifest media type for image {} with reference {}", name, reference);
-
-        var manifestPath = getManifestPath(name, reference);
-        try (var reader = Files.newBufferedReader(manifestPath); var jsonReader = Json.createReader(reader)) {
-
+    private String getMediaType(byte[] content) {
+        try (var inputStream = new ByteArrayInputStream(content); var jsonReader = Json.createReader(inputStream)) {
             var mediaType = jsonReader.readObject().getString("mediaType");
             logger.debug("Manifest media type is {}", mediaType);
 
@@ -117,37 +72,6 @@ public class ManifestService {
         } catch (IOException e) {
             throw new ManifestInvalidException(e);
         }
-    }
-
-    public void checkExistence(String name, String reference) {
-        logger.debug("Check existence manifest with reference {}", reference);
-
-        if(getManifestPath(name, reference) == null) {
-            throw new NameUnknownException();
-        }
-    }
-
-    public byte[] getContent(String name, String reference) {
-        logger.debug("Get manifest for image {} with reference {}", name, reference);
-
-        try {
-            return Files.readAllBytes(getManifestPath(name, reference));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Path getManifestPath(String name, String reference) {
-        if(reference.contains(":")) {
-            var hash = reference.split(":")[1];
-
-
-            var hashPath = path.resolve(hash + ".json");
-            return Files.exists(hashPath) ? hashPath : null;
-        }
-        var tagPath = path.resolve(name + "." + reference + ".json");
-        return Files.exists(tagPath) ? tagPath : null;
-
     }
 
 }
