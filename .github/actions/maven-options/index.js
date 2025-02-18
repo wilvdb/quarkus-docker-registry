f21d719313679195106921758ed3ba2a0a3a57e0
Wilfried Vandenberghe <wilfried.vandenberghe@ctie.etat.lu>
2025-02-18 11:54:34 +0100

    feat: enrich mvn options action

--- .github/actions/maven-options/index.js
+++ .github/actions/maven-options/index.js
@@ -3,9 +3,15 @@ const core = require('@actions/core');
 try {
     const native = core.getInput('native');
     const defaultOptions = '-B package -DskipTests=true -Dstyle.color=always -Djansi.force=true --no-transfer-progress';
-    const mvnOptions = native === 'true' ? `${defaultOptions} -Dnative -Dquarkus.native.sources-only=true`: defaultOptions
+    const mvnOptions = native === 'true' ? `${defaultOptions} -Dnative -Dquarkus.native.sources-only=true`: defaultOptions;
     core.setOutput("mvn-options", mvnOptions);
+    const targets = native === 'true' ? 'registry/target/**/*.jar': 'registry/target/quarkus-app/**/*.jar';
+    core.setOutput("targets", targets);
+    const artifactsName = native === 'true' ? 'native-jars': 'jars';
+    core.setOutput("artifacts-name", artifactsName);
     console.log(`=> mvn-options: ${mvnOptions}`);
+    console.log(`=> targets: ${targets}`);
+    console.log(`=> artifacts-name: ${artifactsName}`);
 } catch (error) {
     core.setFailed(error.message);
 }
